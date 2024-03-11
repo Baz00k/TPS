ff6835d77fb5338cee168086d2eeb8e325874d2b
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-03-11 14:42:23 +0100

    Improve stats handling and character controllers (#6)
    * refactor: make character controllers more generic, use namespaces for organisation
    
    * refactor: rename file to match class name
    
    * chore: yield after trying to find the player to avoid initial wait time
    
    * chore: refactor enemy to use character controller, improve AI (stop at distance) and use rotation agent instead of rotating GameObject manually
    
    * refactor: remove character movement from base character controller as AI agents do not use it
    
    * fix: sprite rendering
    
    * feat: add collider to wall
    
    * feat: add collider to wall

---
+++ Assets/Scripts/Characters/CharacterStats.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace TPS.Characters
+{
+    [Serializable]
+    public class CharacterStats
+    {
+        [Tooltip("The maximum health of the character")]
+        public int MaxHealth = 100;
+
+        [Tooltip("The movement speed of the character in units per second")]
+        public int MovementSpeed = 10;
+    }
+}
---
+++ Assets/Scripts/Weapons/ProjectileWeaponController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace TPS.Weapons
+{
+    public class ProjectileWeaponController : BaseInventoryItem
+    {
+        [SerializeField] private GameObject projectilePrefab;
+        [SerializeField] private Transform firePoint;
+
+        [SerializeField]
+        [Range(0.5f, 10f)]
+        private float fireRate = 1f;
+
+        [SerializeField]
+        [Range(1f, 100f)]
+        private float projectileSpeed = 10f;
+
+        [SerializeField]
+        [Range(1f, 100f)]
+        private float range = 10f;
+
+        [SerializeField]
+        [Range(1f, 100f)]
+        private float damage = 10f;
+
+        private float fireRateTimer = 0f;
+
+        public override void Use() => Fire();
+
+        private void Fire()
+        {
+            if (Time.time > fireRateTimer)
+            {
+                GameObject bullet = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+                BulletController bulletController = bullet.GetComponent<BulletController>();
+                bulletController.SetStats(projectileSpeed, range, damage);
+
+                fireRateTimer = Time.time + 1f / fireRate;
+            }
+        }
+    }
+}
