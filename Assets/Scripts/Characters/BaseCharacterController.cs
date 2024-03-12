37bce7d939c4cce645d6e5efb6e62b9cd7190933
Ksardess <106996347+Ksardess@users.noreply.github.com>
2024-03-12 12:44:49 +0100

    Add Healthbar with simple test mechanics (#3)
    * Add Healthbar with simple test mechanics
    
    * Added a script for losing health after touching an object
    
    * chore(deps): add unity git merge package for easier scene conflict management
    
    * moved the code from PlayerStats to CharacterStatsHandler.
    Deleted PlayersStats
    
    * I placed healthBars to the top left corner and made a Ui prefab
    
    * chore: remove sandbox from build
    
    * refactor: move sprites to sprites folder
    
    * fix: move from old unity input to new unity input system
    
    * refactor: move file to UI folder
    
    * feat: create CharacterHealthHandler class for managing character health
    
    * chore: use new character health handler
    
    ---------
    
    Co-authored-by: Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>

--- Assets/Scripts/Characters/BaseCharacterController.cs
+++ Assets/Scripts/Characters/BaseCharacterController.cs
@@ -4,15 +4,18 @@ namespace TPS.Characters
 {
     [RequireComponent(typeof(CharacterStatsHandler))]
     [RequireComponent(typeof(CharacterInventoryHandler))]
+    [RequireComponent(typeof(CharacterHealthHandler))]
     public abstract class BaseCharacterController : MonoBehaviour
     {
         protected CharacterStatsHandler StatsHandler { get; private set; }
         protected CharacterInventoryHandler InventoryHandler { get; private set; }
+        protected CharacterHealthHandler HealthHandler { get; private set; }
 
         protected virtual void Awake()
         {
             StatsHandler = GetComponent<CharacterStatsHandler>();
             InventoryHandler = GetComponent<CharacterInventoryHandler>();
+            HealthHandler = GetComponent<CharacterHealthHandler>();
         }
     }
 }
--- Assets/Scripts/Characters/CharacterStatsHandler.cs
+++ Assets/Scripts/Characters/CharacterStatsHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace TPS.Characters
 {
@@ -7,22 +8,34 @@ namespace TPS.Characters
         [SerializeField]
         [Tooltip("The default stats of the character")]
         private CharacterStats baseStats;
-
         public CharacterStats CurrentStats { get; private set; }
+
+        [Header("Events")]
+        [SerializeField] private UnityEvent<CharacterStats> onStatsChanged;
+        public UnityEvent<CharacterStats> OnStatsChanged => onStatsChanged;
 
         private void Awake()
         {
-            CurrentStats = baseStats;
+            ApplyStats(baseStats);
         }
 
         public void ApplyStats(CharacterStats newStats)
         {
             CurrentStats = newStats;
+            LimitStats();
+            onStatsChanged.Invoke(CurrentStats);
         }
 
         public void ResetStats()
         {
             CurrentStats = baseStats;
+            onStatsChanged.Invoke(CurrentStats);
+        }
+
+        private void LimitStats()
+        {
+            CurrentStats.MaxHealth = Mathf.Clamp(CurrentStats.MaxHealth, 1, CurrentStats.MaxHealth);
+            CurrentStats.MovementSpeed = Mathf.Max(0, CurrentStats.MovementSpeed);
         }
     }
 }
