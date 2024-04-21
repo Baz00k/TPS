788885cf781e923bfe5665643db4a4ac5c9e2b96
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-04-21 12:06:54 +0200

    fix: loading levels in game

--- Assets/Scripts/Extract.cs
+++ Assets/Scripts/Extract.cs
@@ -1,20 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TPS.Characters;
+using UnityEngine.Events;
 
 public class Extract : MonoBehaviour
 {
+    [SerializeField] private UnityEvent onPlayerEnter;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            LoadSceneNumberThree();
+            onPlayerEnter.Invoke();
         }
-    }
-
-    private void LoadSceneNumberThree()
-    {
-        SceneManager.LoadScene(3);
-        SceneManager.LoadScene(1, LoadSceneMode.Additive);
     }
 }
